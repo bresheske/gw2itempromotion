562c7895f8cd51bafa13c6a9f05ebeccf4f39f62
bresheske <bresheske@gmail.com>
2015-06-17 17:42:42 -0500

    asdf

---
+++ Scripts/controllers/itemscontroller.js
@@ -0,0 +1,26 @@
+﻿
+// Define our dependencies - located in app.js. 
+define(['jquery', 'angular', 'itemsrepository'], function ($, angular, itemsservice) {
+
+    // Angular bootstrapping.
+    var app = angular.module('ItemsApp', []);
+    app.controller('ItemsController', function ($scope) {
+
+        $scope.Loaded = false;
+        $scope.recipies = [];
+
+        $scope.refresh = function () {
+            $scope.Loaded = false;
+            itemsservice.getItems(function (items) {
+                $scope.recipies = items;
+                $scope.Loaded = true;
+                $scope.$apply();
+            });
+        };
+
+        $scope.refresh();
+
+    });
+
+});
+
