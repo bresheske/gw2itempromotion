93e3cedf034d0c2de42b5510a38fc9e6158c76a4
bresheske <bresheske@gmail.com>
2015-07-09 18:24:48 -0500

    asdf

--- Scripts/controllers/ItemsController.js
+++ Scripts/controllers/ItemsController.js
@@ -2,29 +2,41 @@
 // Define our dependencies - located in app.js. 
 define(['jquery', 'angular', 'itemsrepository'], function ($, angular, itemsservice) {
 
+    var init = function (app) {
+        app.controller('ItemsController', function ($scope) {
+
+            $scope.Loaded = false;
+            $scope.recipies = [];
+
+            $scope.refresh = function () {
+                $scope.Loaded = false;
+                itemsservice.getItems(function (items) {
+                    $scope.recipies = items;
+                    $scope.Loaded = true;
+                    $scope.$apply();
+                });
+            };
+
+            // Boot up the App.
+            $(function () {
+                $scope.refresh();
+            });
+
+        });
+    };
+
+
     // Angular bootstrapping.
     var app = angular.module('ItemsApp', []);
-    angular.element(document).ready(function () { angular.bootstrap(document, ['ItemsApp']); });
+    angular.element(document).ready(function ()
+    {
+        // Now we can finally do stuff!
+        init(app);
 
-    app.controller('ItemsController', function ($scope) {
-
-        $scope.Loaded = false;
-        $scope.recipies = [];
-
-        $scope.refresh = function () {
-            $scope.Loaded = false;
-            itemsservice.getItems(function (items) {
-                $scope.recipies = items;
-                $scope.Loaded = true;
-                $scope.$apply();
-            });
-        };
-
-        // Boot up the App.
-        $(function () {
-            $scope.refresh();
-        });
-
+        // Bootstrap after the controller is created.
+        angular.bootstrap(document, ['ItemsApp']);
     });
+
+    
 });
 
